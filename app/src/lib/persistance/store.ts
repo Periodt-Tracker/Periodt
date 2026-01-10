70ff9257d9f6542c150142112240c66a37584b88
Rabbitminers <rabbitminers2.0@gmail.com>
2026-01-10 20:00:18 +0000

    feat: add asynchronous storage supprot to persisted store

--- app/src/lib/persistance/store.ts
+++ app/src/lib/persistance/store.ts
@@ -15,7 +15,24 @@ export function makePersistedStore<T extends object>(
 	options: PersistedStoreOptions = {},
 ) {
 	const storage = options?.storage ?? preferencesPersistance;
-	const [store, setStore] = createStore(initialValue, { name: options?.name });
+	const [store, setStore] = createStore(initialValue, {
+		name: options?.name,
+	});
+
+	const initial = storage.get<T>(key);
+
+	if (initial instanceof Promise) {
+		initial.then((data) => {
+			if (!data) {
+				setStore(initialValue);
+				return;
+			}
+
+			setStore(data);
+		});
+	} else if (initial) {
+		setStore(initial);
+	}
 
 	createEffect(async () => await storage.set(key, store));
 
