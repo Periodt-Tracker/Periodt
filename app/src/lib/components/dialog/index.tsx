460c7309af86bc4cab6c9b93f81e86c28140a6c3
Rabbitminers <rabbitminers2.0@gmail.com>
2026-01-10 20:02:07 +0000

    feat: add blur to dialog overlay

--- app/src/lib/components/dialog/index.tsx
+++ app/src/lib/components/dialog/index.tsx
@@ -29,7 +29,7 @@ const DialogOverlay = <T extends ValidComponent = "div">(
 	return (
 		<DialogPrimitive.Overlay
 			class={merge(
-				"fixed inset-0 z-50 bg-black/20 data-[expanded]:animate-in data-[closed]:animate-out data-[closed]:fade-out-0 data-[expanded]:fade-in-0",
+				"fixed inset-0 z-50 bg-black/5 backdrop-blur-[3px] data-[expanded]:animate-in data-[closed]:animate-out data-[closed]:fade-out-0 data-[expanded]:fade-in-0",
 				props.class,
 			)}
 			{...rest}
@@ -58,28 +58,12 @@ const DialogContent = <T extends ValidComponent = "div">(
 					"fixed left-1/2 top-1/2 z-50 grid max-h-screen w-[90%] max-w-lg -translate-x-1/2 -translate-y-1/2 gap-4",
 					"overflow-y-auto border bg-background p-8 shadow-lg duration-200",
 					"data-[expanded]:animate-in data-[closed]:animate-out data-[closed]:fade-out-0 data-[expanded]:fade-in-0",
-					"data-[closed]:zoom-out-95 data-[expanded]:zoom-in-95 data-[closed]:zoom-out-95 rounded-[48px]",
+					"data-[closed]:zoom-out-95 data-[expanded]:zoom-in-95 rounded-[48px]",
 					props.class,
 				)}
 				{...rest}
 			>
 				{props.children}
-				<DialogPrimitive.CloseButton class="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[expanded]:bg-accent data-[expanded]:text-muted-foreground">
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						viewBox="0 0 24 24"
-						fill="none"
-						stroke="currentColor"
-						stroke-width="2"
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						class="size-4"
-					>
-						<path d="M18 6l-12 12" />
-						<path d="M6 6l12 12" />
-					</svg>
-					<span class="sr-only">Close</span>
-				</DialogPrimitive.CloseButton>
 			</DialogPrimitive.Content>
 		</DialogPortal>
 	);
