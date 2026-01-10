013ea8ea8cd6094ee5093bf7dc15892a26b9d7f9
Rabbitminers <rabbitminers2.0@gmail.com>
2026-01-10 19:55:45 +0000

    feature: add automatic setup redirect

--- app/src/lib/settings/settings-provider.tsx
+++ app/src/lib/settings/settings-provider.tsx
@@ -1,19 +1,54 @@
-import type { ParentComponent } from "solid-js";
-import { preferencesPersistance } from "../persistance/preferences";
-import { makePersistedStore } from "../persistance/store";
+import {
+	createEffect,
+	createSignal,
+	onMount,
+	type ParentComponent,
+	Show,
+} from "solid-js";
+import { createStore } from "solid-js/store";
+import SetupPage from "@/routes/setup";
+import { preferences } from "../persistance/preferences";
 import { SettingsContext } from "./settings-context";
 import type { PeriodtSettings, SettingsContextType } from "./types";
 
 const SettingsProvider: ParentComponent = (props) => {
-	const [settings, setSettings] = makePersistedStore<PeriodtSettings>(
-		"settings",
-		{ name: "Charlie" },
-		{ storage: preferencesPersistance },
-	);
+	const [loaded, setLoaded] = createSignal(false);
+	const [settings, setSettings] = createStore<PeriodtSettings>({
+		setup_complete: false,
+		name: "",
+		period_length: { lower: 4, upper: 6 },
+		cycle_length: { lower: 26, upper: 28 },
+	});
+
+	onMount(async () => {
+		const stored = await preferences.get<PeriodtSettings>("settings");
+
+		if (stored) {
+			setSettings(stored);
+		}
+
+		setLoaded(true);
+	});
+
+	createEffect(async () => {
+		if (!settings.setup_complete) {
+			return;
+		}
+
+		await preferences.set("settings", settings);
+	});
 
 	const context: SettingsContextType = { settings, setSettings };
 
-	return <SettingsContext.Provider value={context}>{props.children}</SettingsContext.Provider>;
+	return (
+		<SettingsContext.Provider value={context}>
+			<Show when={loaded()} fallback={<div>Loading</div>}>
+				<Show when={settings.setup_complete} fallback={<SetupPage />}>
+					{props.children}
+				</Show>
+			</Show>
+		</SettingsContext.Provider>
+	);
 };
 
 export default SettingsProvider;
--- app/src/lib/settings/types.ts
+++ app/src/lib/settings/types.ts
@@ -1,8 +1,15 @@
 import type { SetStoreFunction, Store } from "solid-js/store";
-import type { Maybe } from "../utilities/types";
+import type { Maybe, NumberRange } from "../utilities/types";
 
 export type PeriodtSettings = {
-	name: Maybe<string>;
+	setup_complete: boolean;
+	name: string;
+	securtiy: Maybe<SecurityMethod>;
+	blank_screen: Maybe<boolean>;
+	lock_on_resume: Maybe<boolean>;
+	show_fertility: Maybe<boolean>;
+	period_length: NumberRange;
+	cycle_length: NumberRange;
 };
 
 export interface SettingsContextType {
@@ -10,3 +17,5 @@ export interface SettingsContextType {
 
 	setSettings: SetStoreFunction<PeriodtSettings>;
 }
+
+export type SecurityMethod = "device" | "pin" | "none";
