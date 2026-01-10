5cd87ca83b1b998527711d221c7df9ac62d46538
Rabbitminers <rabbitminers2.0@gmail.com>
2026-01-10 19:56:07 +0000

    chore: remove unusued persistence utilities

--- app/src/lib/utilities/persistance.ts
+++
@@ -1,8 +0,0 @@
-export const makePersistedStore = <T extends object>(initial: T, key: string) => {
-	const persisted = loadStorage<T>(key, initial);
-	const [storeValue, setStore] = createStore(persisted);
-
-	createEffect(() => saveStorage(key, storeValue));
-
-	return [storeValue, setStore] as const;
-};
