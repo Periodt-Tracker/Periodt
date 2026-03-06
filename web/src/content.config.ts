48b4a38545c8c77ee9e14942a3f782a90ee1564f
Rabbitminers <rabbitminers2.0@gmail.com>
2026-03-06 00:28:48 +0000

    feat: implement blog site

---
+++ web/src/content.config.ts
@@ -0,0 +1,17 @@
+import { defineCollection, z } from "astro:content";
+import { glob } from "astro/loaders";
+
+const blog = defineCollection({
+	loader: glob({ base: "./src/content/blog", pattern: "**/*.{md,mdx}" }),
+	schema: ({ image }) =>
+		z.object({
+			title: z.string(),
+			description: z.string(),
+			date: z.coerce.date(),
+			category: z.string(),
+			updated_date: z.coerce.date().optional(),
+			image: image().optional(),
+		}),
+});
+
+export const collections = { blog };
---
+++ web/src/content/blog/first-post copy.md
@@ -0,0 +1,16 @@
+---
+title: 'Julias Post'
+description: 'Lorem ipsum dolor sit amet'
+date: 'Jul 08 2026'
+image: '../../assets/blog-placeholder-3.jpg'
+category: 'Announcement'
+---
+
+Hey bitches aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+
+
+- Test
+- Test
+- Test
+
+- Big J
