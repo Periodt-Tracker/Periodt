b77f407606601072bc1c65ae43d62d25beb7076b
Rabbitminers <rabbitminers2.0@gmail.com>
2026-03-14 18:42:39 +0000

    feat: add latex support to blog

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -4,6 +4,8 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,4 +14,8 @@ export default defineConfig({
 	vite: {
 		plugins: [tailwindcss()],
 	},
+	markdown: {
+		remarkPlugins: [remarkMath],
+		rehypePlugins: [rehypeKatex],
+	}
 });
